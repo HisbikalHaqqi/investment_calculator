4d538cf91c13dac081a0a6d189ee84ea36d86570
HisbikalHaqqi <it.lovers.1998@gmail.com>
2024-12-30 11:17:25 +0700

    fixing-bug-04

---
+++ .next/static/chunks/app/(site)/pricing/page-4545337ca1815880.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[17],{4166:function(e,n,s){Promise.resolve().then(s.t.bind(s,2972,23)),Promise.resolve().then(s.bind(s,6341))}},function(e){e.O(0,[972,356,341,971,117,744],function(){return e(e.s=4166)}),_N_E=e.O()}]);
